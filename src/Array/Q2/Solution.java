ec2c7250079f5871874f86a37d14a19f2afaf11d
st2220 <sihan.tao@imperial.ac.uk>
2021-10-27 16:26:17 +0100

    Solution to max profit of stocks.

---
+++ src/Array/Q2/Solution.java
@@ -0,0 +1,16 @@
+package Array.Q2;
+
+public class Solution {
+  public int maxProfit(int[] prices) {
+    int[][] profits = new int[2][prices.length];
+    profits[0][0] = 0;
+    profits[1][0] = - prices[0];
+    int n = prices.length;
+    for (int i = 1; i < n; i++) {
+      profits[0][i] = Math.max(profits[1][i - 1] + prices[i], profits[0][i - 1]);
+      profits[1][i] = Math.max(profits[1][i - 1], profits[0][i - 1] - prices[i]);
+    }
+
+    return profits[0][n - 1];
+  }
+}
